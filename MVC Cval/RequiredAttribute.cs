fea2f2bfed0b84a900be651421d7ed70482e1e89
Andreas Strid <andreas_strid@bredband.net>
2011-07-02 20:12:38 +0200

    Created project

---
+++ MVC Cval/RequiredAttribute.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
+
+namespace MVC_Cval
+{
+    public class RequiredAttribute : System.ComponentModel.DataAnnotations.RequiredAttribute, IClientValidatable
+    {
+        #region IClientValidatable Members
+
+        public IEnumerable<ModelClientValidationRule> GetClientValidationRules(ModelMetadata metadata, ControllerContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+    }
+}
