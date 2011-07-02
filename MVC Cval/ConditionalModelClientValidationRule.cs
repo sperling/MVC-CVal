7b8f656399cc570d5e333e2c955165a1d866faea
Andreas Strid <andreas_strid@bredband.net>
2011-07-02 20:48:17 +0200

    Added base class for ModelClientValidationRule

---
+++ MVC Cval/ConditionalModelClientValidationRule.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
+
+namespace MVC_Cval
+{
+    internal abstract class ConditionalModelClientValidationRule : ModelClientValidationRule
+    {
+        public ConditionalModelClientValidationRule(string errorMessage, bool validateIfNot, string conditionProperty)
+        {
+            ErrorMessage = errorMessage;
+            ValidationType = "CV" + VType;
+            ValidationParameters.Add("validateifnot", validateIfNot);
+            ValidationParameters.Add("conditionproperty", conditionProperty);
+        }
+
+        protected abstract string VType { get; }
+    }
+}
--- MVC Cval/RequiredAttribute.cs
+++ MVC Cval/RequiredAttribute.cs
@@ -8,13 +8,35 @@ namespace MVC_Cval
 {
     public class RequiredAttribute : System.ComponentModel.DataAnnotations.RequiredAttribute, IClientValidatable
     {
+        /// <summary>
+        /// 
+        /// </summary>
+        public string ConditionProperty { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public bool ValidateIfNot { get; set; }
+
         #region IClientValidatable Members
 
         public IEnumerable<ModelClientValidationRule> GetClientValidationRules(ModelMetadata metadata, ControllerContext context)
         {
-            throw new NotImplementedException();
+            return new[] { new ModelClientValidationRequiredRule(this.FormatErrorMessage(metadata.PropertyName), ValidateIfNot, ConditionProperty) };
         }
 
         #endregion
+
+        class ModelClientValidationRequiredRule : ConditionalModelClientValidationRule
+        {
+            public ModelClientValidationRequiredRule(string errorMessage, bool validateIfNot, string conditionProperty) : base(errorMessage, validateIfNot, conditionProperty)
+            {
+            }
+
+            protected override string VType
+            {
+                get { return "required"; }
+            }
+        }
     }
 }
