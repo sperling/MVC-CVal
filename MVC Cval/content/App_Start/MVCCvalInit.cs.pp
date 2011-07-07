c1dd734abc02439bd3397ebaa0fcd8bb60783277
Andreas Strid <andreas_strid@bredband.net>
2011-07-07 18:50:12 +0200

    Fixed "for real this time" rename...

---
+++ MVC Cval/content/App_Start/MVCCvalInit.cs.pp
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+[assembly: WebActivator.PostApplicationStartMethod(typeof($rootnamespace$.App_Start.MVCCValInit), "PostStart")]
+
+namespace $rootnamespace$.App_Start
+{
+    public static class MVCCValInit
+    {
+        public static void PostStart() 
+        {
+            MVCCval.Helpers.Init();
+        }
+    } 
+}
